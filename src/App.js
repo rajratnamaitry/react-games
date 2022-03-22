9b90684cc3191daf2eb01cfc0a8fa76f64fd8b66
rajratnamaitry <rajratnamaitry@gmail.com>
2022-03-22 16:23:50 +0530

    new game added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Tictac from "./tic-tac/tic-tac";
+import Tiles from "./2048/tiles";
 import './App.css'
 import { useState } from 'react';
 function App() {
@@ -11,9 +12,10 @@ function App() {
     <div className="App">
       <ul>
         <li><div className="active" data-game="tictac" onClick={handleClick}>Tictac</div></li>
+         <li><div data-game="2048" onClick={handleClick} >2048 Game</div></li>
          <li><div data-game="memo" onClick={handleClick} >Memory Game</div></li>
       </ul>
-      { game === 'tictac' ? <Tictac></Tictac> : <div></div> }
+      { game === 'tictac' ? <Tictac></Tictac> : <Tiles></Tiles> }
     </div>
   );
 }
